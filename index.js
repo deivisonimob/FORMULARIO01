472e44ecf817c68c8f79d1e0f60e881a704b70b2
deivisonimob <149898447+deivisonimob@users.noreply.github.com>
2023-11-07 19:59:03 -0300

    Update index.js
    foi atualizado!

--- index.js
+++ index.js
@@ -1,72 +1,228 @@
-const express = require('express');
-const nodemailer = require('nodemailer');
-const bodyParser = require('body-parser');
-const fs = require('fs');
-const app = express();
-const port = process.env.PORT || 3000;
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Agendamento Online</title>
+    
+    <style>
+        body {
+            background-color: #000; /* Cor preta */
+            color: #ffffff; /* Cor do texto */
+            display: flex;
+            flex-direction: column;
+            align-items: center; /* Alterado para 'align-items' para centralizar verticalmente */
+            margin: 0;
+        }
+    
+        #countdown {
+            text-align: center;
+            font-size: 20px;
+            color: orangered;
+            font-family: 'Arial Black', sans-serif;
+        }
+    
+        /* Estilos gerais para o formulário */
+        form {
+            width: 90%; /* Altere a largura do formulário */
+            max-width: 400px; /* Defina um valor máximo de largura */
+            margin: 0 auto;
+            background-color: #000000;
+            padding: 20px;
+            border-radius: 10px;
+            box-shadow: 0 0 10px rgb(0, 0, 0);
+        }
+    
+        h1 {
+            font-family: 'Arial Black', Arial, sans-serif;
+            font-size: 30px;
+            max-width: 400px; /* Defina a largura máxima desejada */
+            color: floralwhite;
+            text-align: center;
+            color: floralwhite;
+            margin: 0 auto; /* Centraliza o h1 */
+        }
+    
+        /* Estilo para rótulos dos campos */
+        label {
+            font-weight: bold;
+            display: block;
+            margin-bottom: 5px;
+        }
+    
+        /* Estilo para campos de entrada de texto e área de texto */
+        .input-padrao {
+            width: 90%;
+            padding: 10px;
+            margin-bottom: 15px;
+            border: 1px solid #ccc;
+            border-radius: 5px;
+            font-size: 16px;
+        }
+    
+        /* Estilo para o botão de envio */
+        .enviar {
+            background-color: #52d108e7;
+            color: #000000;
+            padding: 15px 155px;
+            border: none;
+            border-radius: 5px;
+            font-size: 30px;
+            cursor: pointer;
+            transition: background-color 0.3s;
+        }
+    
+        .enviar:hover {
+            background-color: rgb(146, 255, 73);
+        }
+    
+        /* Estilo para o checkbox */
+        .checkbox {
+            display: block;
+            margin: 10px 0;
+            font-weight: normal;
+        }
+    
+        /* Estilo para o campo de seleção */
+        select {
+            width: 80%;
+            padding: 10px;
+            margin-bottom: 15px;
+            border: 1px solid #f10000;
+            border-radius: 5px;
+            font-size: 16px;
+        }
+    
+        /* Estilo para os radio buttons */
+        input[type="radio"] {
+            margin-right: 10px;
+            text-align: center;
+        }
+    
+        /* Estilos para o rodapé */
+        footer {
+            text-align: center;
+        }
+    
+        /* Estilos para imagens */
+        .imagem-container {
+            text-align: center; margin-bottom: 20px;
+        }
+    
+        .imagem-container img {
+            width: 400px;
+            height: auto;
+            display: block;
+            margin: 0 auto;
+            margin-top: 20px;
+        }
+    
+        /* Estilos para o contêiner de vídeo */
+        .video-container {
+            text-align: center;
+        }
+    
+        .video-container video {
+            width: 400px;
+            height: 400px;
+            display: block;
+            margin: 0 auto;
+        }
+        /* Outros estilos específicos, se necessário */
+    </style>
+    
+</head>
+<body>
+    <main>
+        <h1>Conheça o novo Empreendimento Residencial Condomínio Club na Vila das Belezas - SP</h1>
+        <!-- Inclusão das imagens -->
+        <div class="imagem-container">
+            <img src="https://objectstorage.sa-saopaulo-1.oraclecloud.com/n/grq6lwb4htd1/b/tecimob-production/o/media/377caa23-04ed-420b-8fba-3026d73b17bb/properties/cf77adce-1dab-4ead-a3ce-d703da2fe4e3/images/da3fcd98-cadd-4728-b5f9-3de35c698be21698064081tkru.jpg" alt="Imagem 1">
+            <img src="https://objectstorage.sa-saopaulo-1.oraclecloud.com/n/grq6lwb4htd1/b/tecimob-production/o/media/377caa23-04ed-420b-8fba-3026d73b17bb/properties/cf77adce-1dab-4ead-a3ce-d703da2fe4e3/images/03bbf2d1-66b9-4120-aa7f-2797b0749f7d1698064082Yr53.jpg" alt="Imagem 2">
+            <img src="https://objectstorage.sa-saopaulo-1.oraclecloud.com/n/grq6lwb4htd1/b/tecimob-production/o/media/377caa23-04ed-420b-8fba-3026d73b17bb/properties/cf77adce-1dab-4ead-a3ce-d703da2fe4e3/images/8b9f5b95-1855-463e-851b-94a2c84d65261698064082C85V.jpg" alt="Imagem 3">
+            <img src="https://objectstorage.sa-saopaulo-1.oraclecloud.com/n/grq6lwb4htd1/b/tecimob-production/o/media/377caa23-04ed-420b-8fba-3026d73b17bb/properties/cf77adce-1dab-4ead-a3ce-d703da2fe4e3/images/b0162d69-08e1-4fa7-851d-3cdd7c6a28611698064082uGbd.jpg" alt="Imagem 4">
+            <img src="https://objectstorage.sa-saopaulo-1.oraclecloud.com/n/grq6lwb4htd1/b/tecimob-production/o/media/377caa23-04ed-420b-8fba-3026d73b17bb/properties/cf77adce-1dab-4ead-a3ce-d703da2fe4e3/images/6692997a-a03c-42c0-9c59-4a2ace65954f1698064086uJgm.jpg" alt="Imagem 5">
+            <img src="https://objectstorage.sa-saopaulo-1.oraclecloud.com/n/grq6lwb4htd1/b/tecimob-production/o/media/377caa23-04ed-420b-8fba-3026d73b17bb/properties/cf77adce-1dab-4ead-a3ce-d703da2fe4e3/images/fe727248-33b0-43e7-b933-71aad29fb5ef1698064086hklX.jpg" alt="Imagem 6">
+            <img src="https://objectstorage.sa-saopaulo-1.oraclecloud.com/n/grq6lwb4htd1/b/tecimob-production/o/media/377caa23-04ed-420b-8fba-3026d73b17bb/properties/cf77adce-1dab-4ead-a3ce-d703da2fe4e3/images/5568c345-1c53-4569-ba91-2b02b742fb811698064224XmU5.jpg" alt="Imagem 7">
+            <img src="https://objectstorage.sa-saopaulo-1.oraclecloud.com/n/grq6lwb4htd1/b/tecimob-production/o/media/377caa23-04ed-420b-8fba-3026d73b17bb/properties/cf77adce-1dab-4ead-a3ce-d703da2fe4e3/images/fa139eb1-e35b-45fc-8ca9-ca61a4993e821698064224Z5G0.jpg" alt="Imagem 8">
+            <img src="https://objectstorage.sa-saopaulo-1.oraclecloud.com/n/grq6lwb4htd1/b/tecimob-production/o/media/377caa23-04ed-420b-8fba-3026d73b17bb/properties/cf77adce-1dab-4ead-a3ce-d703da2fe4e3/images/7cc09734-a931-4392-b5ee-77cfd8697c0b1698064160r3up.jpg" alt="Imagem 9">
+            <img src="https://objectstorage.sa-saopaulo-1.oraclecloud.com/n/grq6lwb4htd1/b/tecimob-production/o/media/377caa23-04ed-420b-8fba-3026d73b17bb/properties/cf77adce-1dab-4ead-a3ce-d703da2fe4e3/images/1468e65a-b7a4-49ec-9374-e8ad3355ca911698064160C4pT.jpg" alt="Imagem 10">
+            <img src="https://objectstorage.sa-saopaulo-1.oraclecloud.com/n/grq6lwb4htd1/b/tecimob-production/o/media/377caa23-04ed-420b-8fba-3026d73b17bb/properties/cf77adce-1dab-4ead-a3ce-d703da2fe4e3/images/9798d03c-9753-4aa8-963d-d3a25b4cb9331698064159N7xW.jpg" alt="Imagem 11">
+        
+            <h1>Devido o costume de ofertas da black friday resolvemos manter os preços de lançamento, aproveite!</h1>
+            
+        </div>
+            
+        <div class="video-container">
+            <video width="640" controls autoplay preload="auto" loop>
+                <source src="video01.mp4" type="video/mp4">
+                <!-- Inclua outros formatos de vídeo suportados (WebM, Ogg) aqui -->
+                Seu navegador não suporta o elemento de vídeo.
+            </video>
+            <h1>Solicite aqui mais informações de preços e financiamento:</h1>
+        </div>
+        <form action="https://formspree.io/f/xvojywna" method="POST">
+            <label for="nome">Nome:</label>
+            <input type="text" id="nome" name="name" class="input-padrao" autocomplete="name" required placeholder="insira seu nome aqui">
+        
+            <label for="email">E-mail:</label>
+            <input type="email" id="email" name="email" class="input-padrao" autocomplete="email" required placeholder="insira seu email aqui">
+        
+            <label for="contato">Contato WhatsApp:</label>
+            <input type="tel" id="contato" name="contato" class="input-padrao" autocomplete="cc-number" required placeholder="(xx) xxxxx-xxxx">
+        
+            <label for="mensagem">Se você procura alguma informação específica, aproveite o campo abaixo para enviar sua mensagem:</label>
+            <textarea cols="20" rows="5" id="mensagem" name="mensagem" class="input-padrao"></textarea>
+        
+            <fieldset>
+                <legend>Qual horário você prefere receber atendimento?</legend>
+                <select id="horario" name="horario"> <!-- Adicionado o atributo "id" para o campo de seleção -->
+                    <option>Manhã</option>
+                    <option>Tarde</option>
+                    <option>Noite</option>
+                </select>
+            </fieldset>
+            <fieldset>
+                <legend>Gostaria de deixar pré-agendado uma visita?</legend>
+                <select id="visita" name="visita">
+                    <option value="nao">Não</option>
+                    <option value="sim">Sim</option>
+                </select>
 
-app.use(bodyParser.urlencoded({ extended: true }));
+                <!-- Campos de seleção de data e horário (inicialmente ocultos) -->
+                <div id="data-horario-fields" style="display: none;">
+                    <label for="data">Selecione a data:</label>
+                    <input type="date" id="data" name="data">
 
-// Carrega o arquivo JSON quando a página é carregada
-app.use(express.static(__dirname)); // Servir arquivos estáticos do diretório atual
+                    <label for="horario">Selecione o horário:</label>
+                    <input type="time" id="horario" name="horario">
+                </div>                        
+            </fieldset>
+            <input type="submit" value="Enviar" class="enviar" style="margin-top: 10px;">
+        </form>
+        
+        <footer>
+            <img src="logo.png" alt="Logo." width="100">
+            <p class="copyright">&copy; Copyright DEIVISON - 2023</p>
+        </footer>
+    </main>
+    
+    <script>
+        
+        const visitaSelect = document.getElementById('visita');
+        const dataHorarioFields = document.getElementById('data-horario-fields');
 
-app.get('/', (req, res) => {
-  const rawData = fs.readFileSync('dados.json'); // Lê o arquivo JSON
-  const jsonData = JSON.parse(rawData);
-  if (jsonData.dados && jsonData.dados.length > 0) {
-    const dadosDoJSON = jsonData.dados[0];
-    res.send(`
-      <form action="/enviar-email" method="POST">
-        <input type="text" name="nome" value="${dadosDoJSON.nome}">
-        <input type="email" name="email" value="${dadosDoJSON.email}">
-        <input type="tel" name="telefone" value="${dadosDoJSON.telefone}">
-        <textarea name="mensagem">${dadosDoJSON.mensagem}</textarea>
-        <select name="horario">
-          <option value="manha">Manhã</option>
-          <option value="tarde">Tarde</option>
-          <option value="noite">Noite</option>
-        </select>
-        <button type="submit">Enviar</button>
-      </form>
-    `);
-  } else {
-    res.send('Arquivo JSON vazio ou mal formatado.');
-  }
-});
-
-// Rota para enviar o email
-app.post('/enviar-email', (req, res) => {
-  const { nome, email, telefone, mensagem, horario } = req.body;
-
-  // Configurar o transporte de email
-  const transporter = nodemailer.createTransport({
-    service: 'seu-servico-de-email', // Por exemplo, 'Gmail'
-    auth: {
-      user: 'seu-email@gmail.com',
-      pass: 'sua-senha',
-    },
-  });
-
-  // Detalhes do email
-  const mailOptions = {
-    from: 'seu-email@gmail.com',
-    to: 'email-de-destinatario@example.com',
-    subject: 'Novo formulário de contato',
-    text: `Nome: ${nome}\nE-mail: ${email}\nTelefone: ${telefone}\nHorário de atendimento: ${horario}\nMensagem: ${mensagem}`,
-  };
-
-  // Enviar email
-  transporter.sendMail(mailOptions, (error, info) => {
-    if (error) {
-      console.error('Erro no envio do email:', error);
-      res.status(500).send('Erro no envio do email');
-    } else {
-      console.log('Email enviado: ' + info.response);
-      res.send('Email enviado com sucesso');
-    }
-  });
-});
-
-app.listen(port, () => {
-  console.log(`Servidor está rodando na porta ${port}`);
-});
+        visitaSelect.addEventListener('change', function () {
+            if (visitaSelect.value === 'sim') {
+                // Se "Sim" for selecionado, mostre os campos de seleção de data e horário
+                dataHorarioFields.style.display = 'block';
+            } else {
+                // Caso contrário, oculte os campos de seleção de data e horário
+                dataHorarioFields.style.display = 'none';
+            }
+        });
+    </script>
+   
+</body>
+</html>
